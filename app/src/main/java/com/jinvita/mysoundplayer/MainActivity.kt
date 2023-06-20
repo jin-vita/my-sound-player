fb0657e77bdafc0d209c5dedd0f32845e35be11a
jin-vita <jhj9144@gmail.com>
2023-06-21 08:50:12 +0900

    test lazy init

--- app/src/main/java/com/jinvita/mysoundplayer/MainActivity.kt
+++ app/src/main/java/com/jinvita/mysoundplayer/MainActivity.kt
@@ -9,14 +9,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var mediaPlayer1: MediaPlayer
     private lateinit var mediaPlayer2: MediaPlayer
+    private val mediaPlayer11 by lazy {
+        MediaPlayer.create(this, R.raw.traffic).apply { setOnCompletionListener { start() } }
+    }
+    private val mediaPlayer22 by lazy {
+        MediaPlayer.create(this, R.raw.traffic2).apply { setOnCompletionListener { start() } }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initializeMediaPlayer()
 
-        binding.buttonOne.setOnClickListener { playOrPauseAudio1() }
-        binding.buttonTwo.setOnClickListener { playOrPauseAudio2() }
+        binding.buttonOne.setOnClickListener { playOrPauseAudio11() }
+        binding.buttonTwo.setOnClickListener { playOrPauseAudio22() }
     }
 
     private fun initializeMediaPlayer() {
@@ -24,6 +31,28 @@ class MainActivity : AppCompatActivity() {
         mediaPlayer2 = MediaPlayer.create(this, R.raw.sample2)
         mediaPlayer1.setOnCompletionListener { binding.buttonOne.text = "1번 재생 완료" }
         mediaPlayer2.setOnCompletionListener { binding.buttonTwo.text = "2번 재생 완료" }
+    }
+
+    private fun playOrPauseAudio11() {
+        if (mediaPlayer11.isPlaying) {
+            mediaPlayer11.pause()
+            binding.buttonOne.text = "1번 일시 정지"
+        } else {
+            mediaPlayer11.start()
+            binding.buttonOne.text = "1번 재생 중"
+        }
+
+    }
+
+    private fun playOrPauseAudio22() {
+        if (mediaPlayer22.isPlaying) {
+            mediaPlayer22.stop()
+            mediaPlayer22.prepare()
+            binding.buttonTwo.text = "2번 정지"
+        } else {
+            mediaPlayer22.start()
+            binding.buttonTwo.text = "2번 재생 중"
+        }
     }
 
     private fun playOrPauseAudio1() {
@@ -51,6 +80,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        mediaPlayer11.release()
+        mediaPlayer22.release()
         mediaPlayer1.release()
         mediaPlayer2.release()
     }
